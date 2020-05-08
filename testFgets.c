e94f470bcf41879127a28584bb18f3df7c94709f
guilamb <guilhanebourgin@gmail.com>
2020-05-08 15:26:11 +0200

    Co-authored-by: lucas hottin <lucashottin3@gmail.com>

--- testFgets.c
+++ testFgets.c
@@ -29,7 +29,6 @@ int gets(char fileName[257]){ //taille max des noms de fichiers sous unix
 
      }else if(nbLigne == 2){
      	int etatInitial =  (int)*str[1] - '0';
-     	//printf("%lu\n");
      	if(estChiffre(str[1]) == 0 && etatInitial>=0 && etatInitial<=4 && str[1][1]=='\n'){
      		puts(str[1]);
      	}else{

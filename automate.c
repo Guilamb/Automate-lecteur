67a36aeab8732d8cd0795cbe22a314bc9897ab09
guilamb <guilhanebourgin@gmail.com>
2020-05-08 15:49:51 +0200

    merci

--- automate.c
+++ automate.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 typedef struct Etats{
-	int numero;
-	int entrees[5];
-	int sorties[5];
-
+	int numeros;
+	char trans[5][5];
 }Etat; 
 
 
 typedef struct Automates{
 	Etat etats[5];
 	int initial; //on prends que son numeros, on peut le retrouver apres dans la liste des etats
-	int transitions[5][5][5];
+	int acceptant[5];
 
 }Automate;
